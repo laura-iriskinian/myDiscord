e3c4321424a2394ae6cda53b79ab54d562b0cf1a
Elodie Boweren <elodie.boweren@gmail.com>
2025-04-23 14:47:51 +0200

    feat: improve connection page, first page with choice, then sign in or register page. Back button working

--- include/gui.h
+++ include/gui.h
@@ -1,6 +1,13 @@
 #ifndef GUI_H
 #define GUI_H
 
-void on_app_activate(GtkApplication *app, gpointer user_data);
+#include <gtk/gtk.h>
+
+void show_choice_screen(GtkApplication *app);
+void show_signin_form(GtkWindow *window);
+void show_register_form(GtkWindow *window);
+void load_css(void);
+void go_back_to_choice_screen(GtkWindow *window);
+void show_choice_screen(GtkApplication *app);
 
 #endif
